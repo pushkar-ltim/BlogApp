600b80ac14ee3db567ce7d3f409b77559347edff
Pushkar Nagpure <pushkar.nagpure@ltimindtree.com>
2023-11-08 08:52:43 +0530

    added basic styling to the blog-card component

--- src/app/blog-app/components/home/blog-card/blog-card.component.ts
+++ src/app/blog-app/components/home/blog-card/blog-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
   styleUrls: ['./blog-card.component.scss']
 })
-export class BlogCardComponent {
+export class BlogCardComponent implements OnInit, OnDestroy {
+
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
 
 }
