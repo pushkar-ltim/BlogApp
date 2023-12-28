8083b6e24964af5b5b6fbb62cf864976ed3d05db
Pushkar Nagpure <pushkar.nagpure@ltimindtree.com>
2023-12-28 16:17:18 +0530

    changes

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { HttpService, HttpVerbs } from '../http/http.service';
 @Injectable()
 export class AuthService {
 
-  UserControllerUrl = 'User'
+  UserControllerUrl = '/User'
 
   constructor(
     private http: HttpService
@@ -16,11 +16,33 @@ export class AuthService {
   ) {
     let url = this.UserControllerUrl + '/authenticate';
     let authReqBody: AuthReqBody = {
-      userName : userName,
+      userName: userName,
       password: password
     }
     return this.http.request(HttpVerbs.POST, url, authReqBody)
   }
+
+  // private setSession(authResult) {
+  //   localStorage.setItem('id_token', authResult.idToken);
+  // }
+
+  // logout() {
+  //   localStorage.removeItem("id_token");
+  // }
+
+  // public isLoggedIn() {
+  //   return moment().isBefore(this.getExpiration());
+  // }
+
+  // isLoggedOut() {
+  //   return !this.isLoggedIn();
+  // }
+
+  // getExpiration() {
+  //   const expiration = localStorage.getItem("expires_at");
+  //   const expiresAt = JSON.parse(expiration);
+  //   return moment(expiresAt);
+  // }
 }
 
 interface AuthReqBody {
