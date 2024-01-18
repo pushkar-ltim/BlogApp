187e855fd1a7ada80450f3cd0ddcd06e96a768da
Pushkar Nagpure <pushkar.nagpure@ltimindtree.com>
2024-01-18 14:20:24 +0530

    register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpService, HttpVerbs } from 'src/app/services/http/http.service';
 
 @Component({
   selector: 'app-register',
@@ -8,14 +10,46 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
 
-  constructor(
-    private router:Router
-  ) {
+  form: FormGroup = new FormGroup({
+    nameOfUser: new FormControl('', Validators.required),
+    userName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email),
+    phone: new FormControl(''),
+    password: new FormControl('', Validators.required),
+    passwordConfirmation: new FormControl('', Validators.required)
+  })
 
+  constructor(
+    private router:Router,
+    private httpService: HttpService,
+  ) {
   }
 
-  onSubmit() {}
+  onSubmit() {
+    let user = {
+      nameOfTheUser: this.form.value.nameOfUser,
+      email: this.form.value.email,
+      username: this.form.value.userName,
+      phone: this.form.value.phone,
+      password: this.form.value.password,
+      passwordConfirmation: this.form.value.passwordConfirmation,
+    }
+
+    if(user.password !== user.passwordConfirmation) {
+      console.error("passwords do not match");
+      this.form.reset();
+      return;
+    }
+
+    let url = 'User/CreateUser';
+
+    this.httpService.request(HttpVerbs.POST, url, user)?.subscribe(res => {
+      this.routeToLogin();
+    });
+  }
+
   routeToLogin() {
     this.router.navigate(['/login'])
   }
+  
 }
